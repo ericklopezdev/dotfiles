dc59053d886f904da1a077959a35b628aaef737f
Erick L. <ecklpz@gmail.com>
2026-02-22 14:09:11 -0500

    feat(nvim): add starter nvim config

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+return {
+  "metalelf0/black-metal-theme-neovim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("black-metal").setup({
+      theme = "taake",
+      term_colors = true,
+    })
+    require("black-metal").load()
+  end,
+}
